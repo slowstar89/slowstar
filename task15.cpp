a7146341c0dd1917ca855ffcac66438f13bce5da
wert <dfgh@gmail.com>
2019-11-15 18:55:16 +0500

    +10

---
+++ task15.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() {
+    int n;
+    std::cout << "Введите число: " << std::endl;
+    std::cin >> n;
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        if ( (i >= j) || (i > (n - j - 1)) ) {
+            std::cout << "*";
+        } else {
+            std::cout << ".";
+        }
+    }
+        std::cout << std::endl;
+}
+return 0;
+} 
--- task9_var.cpp
+++ task9_var.cpp
@@ -4,25 +4,31 @@ int main() {
     int n;
     std::cout << "Введите число: " << std::endl;
     std::cin >> n;
-    
+
+    int s = 0;
+
     for (int i = n; i > 0; i -= 2) {
-        for (int j = 0; j < (n - i) / 2; j++) {
+        for (int j = 0; j < s; j++) {
             std::cout << " ";
         }
         for (int j = 0; j < i; j++) {
             std::cout << "*";
         }
         std::cout << std::endl;
+        s++;
     }
 
+    s -= 2;
+
     for (int i = 3; i <= n ; i += 2) {
-        for (int j = 0; j < (n - i) / 2; j++) {
+        for (int j = 0; j < s; j++) {
             std::cout << " ";
         }
         for (int j = 0; j < i; j++) {
             std::cout << "*";
         }
         std::cout << std::endl;
+        s--;
     }
 
     return 0;
