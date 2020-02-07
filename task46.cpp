5433aae2c1c901a2b665d10d262590736e3fd987
wert <dfgh@gmail.com>
2020-02-07 18:57:50 +0500

    +46

--- task46.cpp
+++ task46.cpp
@@ -16,23 +16,26 @@ int main () {
     for (int i = 0; i < a.size(); i++) {
         std::cout << i + 1<< ". " << a[i] << std::endl;
     }
-    std::cout << "Введите число, которое будем искать ";
+    std::cout << "Введите число, которое будем искать " << std::endl;
     std::cin >> b;
     end = a.size() - 1;
-    if ((b < a[begin]) || (b > a[end])) {
-        std::cout << "Такого числа нет в массиве ";    
-    }
-    do { 
-        m = (end - begin) / 2;
+    m = end / 2;
+    while (end > begin) {
+       if (a[m] == b) {
+            std::cout << "Номер введенного числа = " << m + 1 << std::endl;    
+            break;
+        }
         if (a[m] > b) {
             end = m;
+            m = (end - begin) / 2;
         }
         if (a[m] < b) {
             begin = m;
+            m = begin + (end - begin) / 2;
         }
-    } while (a[m] != b);
-    if (a[m] == b) {
-        std::cout << "Номер введенного числа = " << m + 1;    
-        }
+    }
+    if (end == begin) {
+        std::cout << "Такого числа нет" << std::endl;
+    }    
     return 0;
 }
