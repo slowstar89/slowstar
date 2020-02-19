e43ba56137d7f4a3e3498bb8f7768d0d8c51201c
slowstar89 <57183787+slowstar89@users.noreply.github.com>
2020-02-19 12:43:08 +0500

    Update task48.cpp
    he tomado una decisión de este problema

--- task48.cpp
+++ task48.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 #include <string>
-int main () {
-    std::string abc;
-    std::cout << "Введите строку" << std::endl;
-    std::getline(std::cin, abc);
+int main()
+{	std::string la_frasa;
+	std::cout << "Enter a string " << std::endl;
+	std::getline(std::cin, la_frasa);
+	int i = 0, j = la_frasa.size() - 1;
+	bool pal = true;
+	while (i < la_frasa.size() - 1) {
+		if (la_frasa[i] != ' ') {
+			if (la_frasa[j] != ' ') {
+				if (la_frasa[i] == la_frasa[j]) {
+					pal = true;
+					i++;
+					j--;
+				}
+				else {
+					pal = false;
+					
+					break;
+				}
+			}
+			else {
+				j--;
+			}
+		}
+		else {
+			i++;
+		}
+	}
+	if (pal == false) {
+		std::cout << "This is not palindrom " << std::endl;
+	}
+	else {
+		std::cout << "This is palindrom " << std::endl;
+	}
+	return 0;
+}
     
